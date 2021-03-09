b28b216744d2034d4c87adffe1d66d5017112a18
Juan Carlos <juanrov_@hotmail.com>
2021-03-08 23:19:04 -0300

    feat: db user and CRUD

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,119 @@
+/* eslint-disable class-methods-use-this */
+import User from '../models/User';
+
+class UserController {
+  async show(request, response) {
+    const { username } = request.params;
+
+    const user = await User.findOne({ where: { username } });
+
+    if (!user) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    const { id, name, email, location, avatar, bio } = user;
+
+    return response.json({
+      id,
+      name,
+      email,
+      location,
+      avatar,
+      bio,
+    });
+  }
+
+  async index(request, response) {
+    const user = await User.findAll();
+
+    return response.json(user);
+  }
+
+  async store(request, response) {
+    const { name, email, location, avatar, username, bio } = request.body;
+
+    const useremailcheck = await User.findOne({ where: { email } });
+
+    if (useremailcheck) {
+      return response.status(401).json({ error: 'not authorized' });
+    }
+
+    const usernamecheck = await User.findOne({ where: { email } });
+
+    if (usernamecheck) {
+      return response.status(401).json({ error: 'not authorized' });
+    }
+
+    const { id } = await User.create({
+      name,
+      email,
+      location,
+      avatar,
+      username,
+      bio,
+    });
+    return response.json({
+      id,
+      name,
+      email,
+      location,
+      avatar,
+      username,
+      bio,
+    });
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+
+    const user = await User.findByPk(id);
+
+    if (!user) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    const { name, email, location, avatar, username, bio } = request.body;
+
+    const changedEmail = email !== user.email;
+
+    const emailAlreadyTaken = null;
+
+    if (changedEmail) {
+      const emailAlreadyTaken = await User.findOne({
+        where: { email },
+      });
+    }
+
+    if (emailAlreadyTaken) {
+      return response.status(401).json({ error: 'not authorized' });
+    }
+
+    await user.save();
+
+    return response.json({
+      name,
+      email,
+      location,
+      avatar,
+      username,
+      bio,
+    });
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const user = await User.findOne({ where: { id } });
+
+    if (!user) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+    await User.destroy({
+      where: { id },
+    });
+
+    return response.json({ message: 'user deleted' });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ messages: 'Hello World!' });
-});
+routes.get('/users/:username', UserController.show);
+routes.get('/users/', UserController.index);
+
+routes.post('/users', UserController.store);
+
+routes.put('/users/:id', UserController.update);
+
+routes.delete('/users/:id', UserController.delete);
 
 export default routes;
